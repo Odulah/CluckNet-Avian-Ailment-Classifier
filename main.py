f6cb17e483c158fa590a95fb75dd40ab06e70feb
Odulah <aadim0311@gmail.com>
2023-07-23 19:40:31 +0530

    data ingestion completed

---
+++ main.py
@@ -0,0 +1,12 @@
+from Avian_Ailment_classifier import logger
+from Avian_Ailment_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
